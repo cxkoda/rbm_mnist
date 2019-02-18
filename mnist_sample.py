678cb75758047b03e0f49fac28299ac26d660a8c
David Huber <david.huber@uibk.ac.at>
2019-02-18 17:59:48 +0100

    Changed Mnist sample display

--- mnist_sample.py
+++ mnist_sample.py
@@ -5,10 +5,10 @@ import numpy as np
 
 
 if __name__ == "__main__":
-	rbm = RBM(filename="mnist.rbm")
+	rbm = RBM(filename="mnist_300.rbm", randomSeed=1234)
 	sampler = gibbsSampler(rbm)
 
-	nSamples = 200
+	nSamples = 10
 
 	hidden = np.random.random_integers(0, 1, size=(rbm.nHidden, nSamples))
 
@@ -19,6 +19,14 @@ if __name__ == "__main__":
 
 	visibles = rbm.probVisibleGivenHidden(1, hidden)
 
+
+	for i, visible in enumerate(visibles.T):
+		plt.figure()
+		plt.imshow(visible.reshape((28,28)))
+
+
+	plt.show()
+	exit(1)
 	for i, visible in enumerate(visibles.T):
 		print(i)
 		plt.axis('off')
